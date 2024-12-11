1d3a6a9fa8f132e526052630b2fc01b93e2b1eff
Tomer Peceniski <tomerpeceniski@gmail.com>
2024-12-11 19:10:35 +0200

    Database Instatiation

---
+++ src/main/java/com/net/workshopmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.net.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.net.workshopmongo.domain.User;
+import com.net.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void run(String... args) {
+        repository.deleteAll();
+        
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        repository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+
+}
