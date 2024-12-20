7f6f0f138493b41aba878d68d9af0defb69c3c91
Tomer Peceniski <tomerpeceniski@gmail.com>
2024-12-20 21:04:59 +0200

    Personalized Query of Post via @Query

--- src/main/java/com/net/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/net/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.net.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.net.workshopmongo.domain.Post;
@@ -10,5 +11,10 @@ import com.net.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
-    List<Post> findByTitleContainingIgnoreCase(String text);
+    // Simple Query MongoDB Method:
+    // List<Post> findByTitleContainingIgnoreCase(String text);
+
+    // Personalized Query:
+    @Query(" { 'title': {$regex: ?0, $options: 'i' } } ")
+    List<Post> searchTitle(String title);
 }
--- src/main/java/com/net/workshopmongo/services/PostService.java
+++ src/main/java/com/net/workshopmongo/services/PostService.java
@@ -22,6 +22,6 @@ public class PostService {
     }
 
     public List<Post> findByTitleContaining(String text) {
-        return repository.findByTitleContainingIgnoreCase(text);
+        return repository.searchTitle(text);
     }
 }
